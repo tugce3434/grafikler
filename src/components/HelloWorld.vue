ae629d17db6c16c2fd59027dd76c745f00cd834d
tugce3434 <tugcenurdaglar@gmail.com>
2020-07-16 00:25:42 +0300

    highchart ve donutchart kullandım

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row mb-4">
+        <div class="col-4">
+          <div class="card">
+            <div class="card-body">
+              <img src="\images\ando.jpg">
+            </div>
+            <div class="card-body">
+              <img src="\images\r2.png" height="170">
+            </div>
+          </div>
+        </div>
+        <div class="col-8">
+          <div class="card">
+            <div class="card-body">
+              <chart></chart>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row mb-4">
+        <div class="col-4">
+          <donut-chart></donut-chart>
+        </div>
+        <div class="col-4">
+          <donut-chart></donut-chart>
+        </div>
+        <div class="col-4">
+          <donut-chart></donut-chart>
+        </div>
+      </div>
+     
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import Chart from "@/components/Chart";
+  import DonutChart from "@/components/DonutChart";
+
+
+  export default {
+  name: 'HelloWorld',
+  components: {
+    Chart,
+    DonutChart
+  }
+
+}
+</script>
+
+<style >
+
+</style>
---
+++ src/components/Veriler.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <p>{{ dil }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Veriler",
+        props:{
+            dil :{
+                type: String,
+                require: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
